c47f30564dd1623e8a7933e517d923553d1efb02
Krishna kumar chaudhary <ch.krishnakumar01@gmail.com>
2024-02-05 00:40:56 +0530

    initial

---
+++ starting-point/src/js/index.js
@@ -0,0 +1,20 @@
+import {
+  newTodoEventHandler,
+  onLoadEventHandler,
+  removeTodoEventHandler,
+  toggleTodoEventListener,
+} from "./event-handlers";
+window.addEventListener("load", onLoadEventHandler);
+document.addEventListener("change", function (event) {
+  if (event.target.classList.contains("new-todo")) {
+    newTodoEventHandler(event);
+  }
+});
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("delete")) {
+    removeTodoEventHandler(event);
+  }
+  if (event.target.classList.contains("real-checkbox")) {
+    toggleTodoEventListener(event);
+  }
+});
